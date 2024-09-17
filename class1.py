ff0ee8b7645bb83feab8be07c13698463ea4704a
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-09-17 21:40:57 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,19 @@
+a=10
+b=20.0
+c="nithin"
+d=8+2j
+
+print(type(a))
+
+print(type(b))
+
+print(type(c))
+
+print(type(d))
+
+b=int(b) #explicit conversion
+
+print(type(b))
+
+array=[1,2,3]
+print(array[1])
---
+++ conditonals.py
@@ -0,0 +1,67 @@
+'''
+age=18
+
+if age>=18:
+    print("Yes you can vote")
+else:
+    print("You cannot vote ")
+
+
+
+
+a=int(input())
+
+b=int(input())
+
+c=int(input())
+
+if a>b and a>c:
+    print("res",a)
+elif b>c:
+    print("res:",b)
+else:
+    print("res",c)
+
+a=int(input("a:"))
+b=int(input("b:"))
+c=a+b
+if c >= 20:
+    print(a+b)
+else:
+    print(a*b)
+
+
+
+
+
+a=int(input())
+if a>10:
+    print(a+5)
+elif a <= 10:
+    print(a+1)
+
+_____________________________________________________________________
+a=int(input("amount:"))
+if(a == 10):
+    b=input("enter the drink name:")
+    if(b == "sprite"):
+        print("Sprite")
+    elif(b == "thumbsup"):
+        print("thumbsup")
+elif(a == 20):
+    c=input("enter chips name:")
+    if(c == "lays"):
+        print("Here is your LAYS")
+    elif(c =="bingo"):
+        print("here is your bingo")
+else:
+    print("Sorry the give amount cant be accepted")
+
+'''
+
+
+
+
+
+
+
